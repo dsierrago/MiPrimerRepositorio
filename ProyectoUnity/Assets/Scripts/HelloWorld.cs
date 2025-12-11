870ed470452f106e29055901fa3e88bc68daa6ff
INFOX-EQUIPXX\DavidTorrePedralbes <dsierrago@inspedralbes.cat>
2025-12-11 15:24:38 +0100

    Carlo trabaja el "AUdio"

---
+++ ProyectoUnity/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    public void Awake()
+    {
+        Debug.Log("HelloWorldAwake");
+    }
+
+    public void Start()
+    {
+        Debug.Log("HelloWorldStart");
+    }
+}
